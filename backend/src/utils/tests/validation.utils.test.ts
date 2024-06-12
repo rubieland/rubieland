8f79e799609ab26d872cf810df1550849b5370f7
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-13 00:10:33 +0200

    feat(validation utils): 🚧 WIP - add phone number validation utils and TODO because need to add tests

---
+++ backend/src/utils/tests/validation.utils.test.ts
@@ -0,0 +1,15 @@
+// TODO: add tests for isValidFrenchPhoneNumber(
+
+// import { isValidFrenchPhoneNumber } from '../validation.utils';
+
+// const numbersToTest = [
+//   '0612121212',
+//   '06-12-12-13-06',
+//   '+33606060606',
+//   '9',
+//   'abcd',
+//   '1234567890',
+//   '0147200001',
+//   '02 51 44 58 931',
+//   '02 51 44 58 96',
+// ];
