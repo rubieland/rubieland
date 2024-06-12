9dd05e39b6814bfba4861f068b0096fe4eef816f
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-13 00:09:56 +0200

    feat(User Model): 🚧 WIP - add fields, validations, and TODO

--- backend/src/models/User.model.ts
+++ backend/src/models/User.model.ts
@@ -1,14 +1,60 @@
-import { Schema, model } from 'mongoose';
+import { Document, Schema, model } from 'mongoose';
 
-const userSchema = new Schema(
+interface UserInput extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  phone?: string;
+  avatar?: string;
+}
+
+/**
+ * TODO:
+ * add validators
+ * add validation messages with i18n
+ * add pre('save') middleware to hash password, comparePasswords method, createJWT method
+ */
+
+const userSchema = new Schema<UserInput>(
   {
-    name: {
+    firstName: {
       type: String,
       required: true,
+      trim: true,
+      maxLength: 255,
+      minlength: 2,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 255,
+      minlength: 2,
     },
     email: {
       type: String,
       required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      maxLength: 255,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 255,
+      minLength: 6,
+    },
+    phone: {
+      type: String,
+      trim: true,
+      minlength: 10,
+    },
+    avatar: {
+      type: String,
+      trim: true,
     },
   },
   { timestamps: true }
