b63ff50a6e8e9009fb497960c40b4104a8463b43
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-13 23:11:09 +0200

    feat(UserModel): add validators

--- backend/src/models/User.model.ts
+++ backend/src/models/User.model.ts
@@ -1,6 +1,9 @@
 import { Document, Schema, model } from 'mongoose';
+import { regexes } from './validators/validators';
+import { isEmail } from 'validator';
+import { isValidFrenchPhoneNumber } from '../utils/validation.utils';
 
-interface UserInput extends Document {
+export interface UserInput {
   firstName: string;
   lastName: string;
   email: string;
@@ -11,26 +14,33 @@ interface UserInput extends Document {
 
 /**
  * TODO:
- * add validators
- * add validation messages with i18n
+ * add validators for avatar (define accepted file formats, sizes...)
  * add pre('save') middleware to hash password, comparePasswords method, createJWT method
  */
 
-const userSchema = new Schema<UserInput>(
+const userSchema = new Schema<UserInput, Document>(
   {
     firstName: {
       type: String,
       required: true,
       trim: true,
-      maxLength: 255,
+      maxLength: 50,
       minlength: 2,
+      validate: {
+        validator: (v: string) => regexes.nameField.test(v),
+        message: `Le prénom est invalide. Il ne peut contenir que des lettres, traits d'union, espaces et apostrophes.`,
+      },
     },
     lastName: {
       type: String,
       required: true,
       trim: true,
-      maxLength: 255,
+      maxLength: 50,
       minlength: 2,
+      validate: {
+        validator: (v: string) => regexes.nameField.test(v),
+        message: `Le nom de famille est invalide. Il ne peut contenir que des lettres, traits d'union, espaces et apostrophes.`,
+      },
     },
     email: {
       type: String,
@@ -38,19 +48,32 @@ const userSchema = new Schema<UserInput>(
       trim: true,
       unique: true,
       lowercase: true,
-      maxLength: 255,
+      maxLength: 254,
+      validate: {
+        validator: (v: string) => isEmail(v),
+        message: `Email incorrect. Veuillez entrer un email valide (mon.adresse@email.com).`,
+      },
     },
     password: {
       type: String,
       required: true,
       trim: true,
-      maxLength: 255,
-      minLength: 6,
+      maxLength: 128,
+      minLength: 8,
+      validate: {
+        validator: (v: string) => regexes.password.test(v),
+        message:
+          'Mot de passe invalide. Les caractères suivants ne sont pas acceptés: < > ( ) { } \\ et `',
+      },
     },
     phone: {
       type: String,
       trim: true,
       minlength: 10,
+      validate: {
+        validator: (v: string) => isValidFrenchPhoneNumber(v),
+        message: `Numéro de téléphone invalide. Veuillez entrer un numéro au format "0XXXXXXXXX" ou "+33XXXXXXXXX"`,
+      },
     },
     avatar: {
       type: String,
