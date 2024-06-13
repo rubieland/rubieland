6e967fd597b01189bf5442a4564c1f6b52563eaa
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-13 23:12:52 +0200

    feat(auth controller): 🚧 WIP: started implementing register

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -1,7 +1,27 @@
 import { Request, Response } from 'express';
+import User, { UserInput } from '../../models/User.model';
+import { isEmpty } from 'validator';
+import { trimData } from '../../utils/string.utils';
 
 export const register = async (req: Request, res: Response) => {
-  res.status(200).json({ route: '/register' });
+  try {
+    const { firstName, lastName, email, password, phone } = trimData(req.body);
+
+    const user: UserInput = {
+      firstName,
+      lastName,
+      email,
+      password,
+      phone,
+    };
+
+    console.log('new user', user);
+    const newUser = await User.create(user);
+    await newUser.save();
+    res.status(201).json({ message: 'Nouvel utilisateur créé !', newUser });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
 };
 export const login = async (req: Request, res: Response) => {
   res.status(200).json({ route: '/login' });
