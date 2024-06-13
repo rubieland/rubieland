d962f44f7abe404c7cf540ee860b7f5535ec2fbf
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-13 23:11:35 +0200

    feat(utils): add trimData function

--- backend/src/utils/string.utils.ts
+++ backend/src/utils/string.utils.ts
@@ -3,3 +3,11 @@
 export const capitalize = (str: string) => {
   return str.replace(str[0], str[0].toUpperCase());
 };
+
+// trim inputs data
+export const trimData = (data: any) => {
+  for (const key in data) {
+    if (typeof data[key] === 'string') data[key] = data[key]?.trim() ?? '';
+  }
+  return data;
+};
