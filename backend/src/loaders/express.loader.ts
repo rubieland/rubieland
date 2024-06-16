2c7bacc9d193d6a9aab273de1d99bdf16f1949e1
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-17 00:25:46 +0200

    refactor(server): separate server,express and db initializations in different files

---
+++ backend/src/loaders/express.loader.ts
@@ -0,0 +1,31 @@
+import i18next from '../config/i18next';
+import { handle as i18nextMiddleware } from 'i18next-express-middleware';
+import cors from 'cors';
+import path from 'path';
+import * as dotenv from 'dotenv';
+import express, { Express } from 'express';
+import authRouter from '../routers/auth.router';
+
+dotenv.config();
+
+const { CLIENT_HOST, CLIENT_PORT } = process.env;
+
+export const loadExpress = async ({ server }: { server: Express }) => {
+  try {
+    // middlewares
+    server.use(i18nextMiddleware(i18next));
+    server.use(express.static(path.join(__dirname, 'public')));
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
+    server.use(
+      cors({
+        origin: `http://${CLIENT_HOST ?? 'localhost'}:${CLIENT_PORT ?? 5173}`,
+      })
+    );
+
+    // routers
+    server.use('/', authRouter);
+  } catch (error) {
+    console.error(error);
+  }
+};
