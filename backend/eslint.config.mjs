487dd522f82a88fd10b90cd3a2863a1550413103
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-18 23:27:38 +0200

    chore: 🚧 WIP: initialize eslint

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,17 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    ignores: ['dist/**', 'coverage/**', '*.cjs'],
+  },
+  {
+    rules: {
+      // TODO: add more rules
+      'object-shorthand': 'error',
+    },
+  },
+];
