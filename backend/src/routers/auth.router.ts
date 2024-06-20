cb3db1586936a644ec96e27dec33cd25b604bf50
Mickaël Mesloub <m.mesloub@beapp.fr>
2024-06-20 15:04:09 +0200

    feat(auth router): add temporary protected route to test verifyToken middleware

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -1,10 +1,16 @@
 import Router, { Express } from 'express';
-import { login, register } from '../controllers/auth/auth.controller';
+import {
+  login,
+  protectedRoute,
+  register,
+} from '../controllers/auth/auth.controller';
 import { verifyToken } from '../middlewares/auth/verifyToken.middleware';
 
 const router: Express = Router();
 
 router.post('/login', login);
-router.post('/register', verifyToken, register);
+router.post('/register', register);
+router.get('/protected', verifyToken, protectedRoute);
+router.get('/');
 
 export default router;
