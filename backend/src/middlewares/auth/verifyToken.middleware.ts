3743d6c0ebf89592d34638bd06394dcd8f9d97a0
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-21 15:29:55 +0200

    refactor(verifyToken): refactor and clean code. add token payload in new req.user object

--- backend/src/middlewares/auth/verifyToken.middleware.ts
+++ backend/src/middlewares/auth/verifyToken.middleware.ts
@@ -1,34 +1,42 @@
 import { NextFunction, Request, Response } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import { env } from '../../loaders/env.loader';
 
 const { JWT_SECRET } = env;
-
-export interface CustomRequest extends Request {
-  token: string | JwtPayload | undefined;
-  isLogged: boolean;
-}
 
 export const verifyToken = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
+  const reqHeaders = req.headers.authorization;
+
+  if (!reqHeaders) {
+    // TODO: use i18n
+    return res
+      .status(401)
+      .json({ error: 'Accès refusé. Veuillez vous authentifier' });
+  }
+
+  const token = reqHeaders.split(' ')[1];
+
+  if (!token) {
+    // TODO: use i18n
+    return res
+      .status(401)
+      .json({ error: 'Accès refusé. Veuillez vous authentifier' });
+  }
+
   try {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-
-    if (!token) {
-      return res.status(401).json({ error: 'Veuillez vous authentifier' });
-    }
-
-    // TODO: maybe change this part
     const decoded = jwt.verify(token, JWT_SECRET);
-    (req as CustomRequest).token = decoded;
-
-    console.log(decoded);
-
+    if (typeof decoded === 'object' && decoded?.id && decoded?.role) {
+      req.user = { id: decoded?.id, role: decoded?.role };
+    }
     next();
-  } catch (err) {
-    res.status(401).send('Veuillez vous reconnecter');
+  } catch (error) {
+    // TODO: use i18n
+    return res
+      .status(403)
+      .json({ error: 'Accès refusé. Veuillez vous reconnecter' });
   }
 };
