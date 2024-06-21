5c1c25a47bb13333247e2459a8137a8bf62a60c0
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-21 15:27:34 +0200

    clean(router): remove mock routes

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -1,16 +1,10 @@
 import Router, { Express } from 'express';
-import {
-  login,
-  protectedRoute,
-  register,
-} from '../controllers/auth/auth.controller';
-import { verifyToken } from '../middlewares/auth/verifyToken.middleware';
+import { login, register } from '../controllers/auth/auth.controller';
 
 const router: Express = Router();
 
 router.post('/login', login);
 router.post('/register', register);
-router.get('/protected', verifyToken, protectedRoute);
 router.get('/');
 
 export default router;
