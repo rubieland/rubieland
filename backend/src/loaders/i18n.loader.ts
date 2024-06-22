d08a3a3e5b6cc1aa206b6dd6552f711f0b6985e7
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-22 17:11:26 +0200

    feat(i18n config): add comment and edit config

--- backend/src/loaders/i18n.loader.ts
+++ backend/src/loaders/i18n.loader.ts
@@ -4,8 +4,16 @@ import { LanguageDetector } from 'i18next-http-middleware';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+/**
+ * DOCS: check i18next crash course for a good implementation
+ * and use of the library features:
+ *  https://www.youtube.com/watch?v=SA_9i4TtxLQ
+ */
+
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
+const defaultLanguage = 'fr';
+const languages = ['fr', 'en'];
 
 export const initI18n = () =>
   new Promise<void>((resolve, reject) => {
@@ -14,8 +22,17 @@ export const initI18n = () =>
       .use(LanguageDetector)
       .init(
         {
-          lng: 'fr',
-          fallbackLng: 'en',
+          lng: defaultLanguage,
+          fallbackLng: defaultLanguage,
+          supportedLngs: languages,
+          preload: languages,
+          detection: {
+            order: ['cookie', 'queryString', 'header'],
+            caches: ['cookie'],
+            lookupQuerystring: 'lang',
+            lookupCookie: 'lang',
+            lookupHeader: 'Accept-Language',
+          },
           backend: {
             loadPath: path.join(
               __dirname,
