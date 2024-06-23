edbd2be5d42b3789ee24aa5b3f70d2a340084825
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-23 16:59:33 +0200

    refactor(validatePhoneNumber utils): before: returned true if phone number is a valid French phone number => now: checks for international phone numbers (format +XX 0000000000)

--- backend/src/utils/validation.utils.ts
+++ backend/src/utils/validation.utils.ts
@@ -1,20 +1,38 @@
-import { parsePhoneNumberFromString } from 'libphonenumber-js';
+import {
+  ParseError,
+  isValidPhoneNumber,
+  parsePhoneNumberWithError,
+} from 'libphonenumber-js';
+import i18n from '../config/i18n';
 
 /**
  * @param phoneNumber(string): phone number to validate
- * @returns(boolean): returns true if the string is a valid French phone number
+ * @returns(boolean): returns true if the string is a valid international phone number
  */
 
-  // remove spaces
-  const normalizedPhoneNumber = phoneNumber.replace(/\s/g, '');
 export const validatePhoneNumber = (phoneNumber: string): boolean => {
+  try {
+    // parse phone number
+    const parsedPhoneNumber = parsePhoneNumberWithError(phoneNumber);
 
-  // parse phone number
-  const parsedPhoneNumber = parsePhoneNumberFromString(
-    normalizedPhoneNumber,
-    'FR'
-  );
+    const formattedNb = parsedPhoneNumber.formatInternational();
+    console.log(formattedNb);
 
-  // return true if parsed phone number is defined and valid
-  return !!parsedPhoneNumber && parsedPhoneNumber.isValid();
+    // return true if parsed phone number is defined and valid
+    return !!parsedPhoneNumber && isValidPhoneNumber(formattedNb);
+  } catch (error: unknown) {
+    if (error instanceof ParseError) {
+      console.error(
+        `An error occured when trying to parse phone number: ${error}`,
+      );
+      process.exit(1);
+    } else {
+      console.error(
+        `An error occured when trying to validate phone number: ${error}`,
+      );
+      process.exit(1);
+    }
+  }
+};
+
 };
