ff04cea9dd7d31c9360b151978e29ab788ac6e02
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-04 17:01:09 +0200

    feat(auth router): add logout route and controller

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -13,6 +13,7 @@ const router: Express = Router();
 router.get('/testAuthMiddleware', authMiddleware, testAuthMiddleware);
 router.post('/login', login);
 router.post('/register', register);
+router.get('/logout', logout);
 router.get('/', getTest);
 
 export default router;
