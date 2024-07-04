0b3556a7cdb191f781462e8602173b4fe179d929
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-04 19:06:47 +0200

    clean(auth router): remove mock routes and controllers, used for tests

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -1,25 +1,9 @@
 import Router, { Express } from 'express';
-import {
-  getTest,
-  login,
-  logout,
-  register,
-  testAuthMiddleware,
-} from '../controllers/auth/auth.controller';
-import { authMiddleware } from '../middlewares/auth/auth.middleware';
-import { isAdminMiddleware } from '../middlewares/admin/isAdmin.middleware';
+import { login, logout, register } from '../controllers/auth/auth.controller';
 
 const router: Express = Router();
-
-router.get(
-  '/testAuthMiddleware',
-  authMiddleware,
-  isAdminMiddleware,
-  testAuthMiddleware,
-);
 router.post('/login', login);
 router.post('/register', register);
 router.get('/logout', logout);
-router.get('/', getTest);
 
 export default router;
