eb51a8bd2e6f2231f6b537e4162f183b1a40f9b9
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-04 13:53:57 +0200

    feat(User validators): WIP add checks for each field except for avatar

--- backend/src/validation/User.validators.ts
+++ backend/src/validation/User.validators.ts
@@ -1,4 +1,13 @@
-import { StrongPasswordOptions } from 'validator';
+import validator, { StrongPasswordOptions } from 'validator';
+import { IUser, UserField } from '../models/types/User.types';
+import {
+  checkFieldFormat,
+  checkMaxLength,
+  checkMinLength,
+  getValidationErrorMessage,
+  validatePhoneNumber,
+} from '../utils/validation.utils';
+import { Reason, UserFieldLengths } from './types/validation.types';
 
 export const regexes = {
   nameField: /^[a-zA-ZÀ-ÖØ-öø-ÿ' -]+$/i, // accepts only letters, hyphens, spaces, and apostrophes
@@ -33,4 +42,96 @@ export const userFieldsLengths: UserFieldLengths = {
     minLength: 9,
     maxLength: 15,
   },
+};
+
+export const checkEmail = (email: string) => {
+  return !validator.isEmail(email)
+    ? getValidationErrorMessage({
+        field: 'email',
+        rule: 'email',
+        reason: Reason.INVALID,
+      })
+    : '';
+};
+
+export const checkPassword = (password: string) => {
+  return !validator.isStrongPassword(password, {
+    ...strongPasswordOptions,
+    returnScore: false,
+  })
+    ? getValidationErrorMessage({
+        field: 'password',
+        rule: 'password',
+        reason: Reason.INVALID,
+      })
+    : '';
+};
+
+export const checkPhone = (phone: string) => {
+  return !validatePhoneNumber(phone)
+    ? getValidationErrorMessage({
+        field: 'phone',
+        rule: 'phone',
+        reason: Reason.INVALID,
+      })
+    : '';
+};
+
+// TODO: refactor to avoid code repetition
+export const checkUserData = (data: IUser) => {
+  const errors: string[] = [];
+
+  for (const [key, value] of Object.entries(data)) {
+    if (key in userFieldsLengths) {
+      const fieldKey = key as UserField;
+
+      // check maxLengths
+      const maxLengthError = checkMaxLength(
+        fieldKey,
+        value,
+        userFieldsLengths[fieldKey].maxLength,
+      );
+
+      if (maxLengthError) errors.push(maxLengthError);
+
+      // check minLengths
+      const minLengthError = checkMinLength(
+        fieldKey,
+        value,
+        userFieldsLengths[fieldKey].minLength,
+      );
+
+      if (minLengthError) errors.push(minLengthError);
+
+      // check firstName and lastName fields
+      if (fieldKey === 'firstName' || fieldKey === 'lastName') {
+        const nameError = checkFieldFormat(fieldKey, value);
+
+        if (nameError) errors.push(nameError);
+      }
+
+      // check email
+      if (fieldKey === 'email') {
+        const emailError = checkEmail(value);
+
+        if (emailError) errors.push(emailError);
+      }
+
+      //check password
+      if (fieldKey === 'password') {
+        const passwordError = checkPassword(value);
+
+        if (passwordError) errors.push(passwordError);
+      }
+
+      // check phone number
+      if (fieldKey === 'phone') {
+        const phoneError = checkPhone(value);
+
+        if (phoneError) errors.push(phoneError);
+      }
+    }
+  }
+
+  return errors;
 };
