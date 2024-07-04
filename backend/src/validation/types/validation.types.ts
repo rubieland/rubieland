274e91eda2b08bf54e2d90bd94c9ba52a4c1510d
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-04 13:46:10 +0200

    feat(types): add types for Validation functions

--- backend/src/validation/types/validation.types.ts
+++ backend/src/validation/types/validation.types.ts
@@ -1,3 +1,5 @@
+import { UserField } from '../../models/types/User.types';
+
 // enum for the reasons of a field validation fail
 export enum Reason {
   INVALID = 'invalid',
@@ -5,3 +7,41 @@ export enum Reason {
   MINLENGTH = 'minLength',
   MAXLENGTH = 'maxLength',
 }
+
+export interface UserFieldLengths {
+  [key: string]: {
+    maxLength: number;
+    minLength: number;
+  };
+}
+
+export type ValidatePhoneNumberFunction = (phoneNumber: string) => boolean;
+
+interface ValidationErrorMessageParams {
+  field: string;
+  rule?: string;
+  minLength?: number;
+  maxLength?: number;
+  reason: Reason;
+}
+
+export type GetValidationErrorMessageFunction = (
+  params: ValidationErrorMessageParams,
+) => string;
+
+export type GetMissingOrEmptyFieldsFunction = (data: any) => string[];
+
+export type GetMissingOrEmptyFieldsErrorMessageFunction = (
+  fields: string[],
+) => string[];
+
+export type FieldLengthValidatorFunction = (
+  field: string,
+  value: string,
+  maxLength: number,
+) => string;
+
+export type FieldFormatValidatorFunction = (
+  fieldType: UserField,
+  fieldValue: string,
+) => string;
