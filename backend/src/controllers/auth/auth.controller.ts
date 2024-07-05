1add391ada4c7109cb3629ff69dbb64580a37276
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-05 18:36:43 +0200

    feat(register): add default avatar in register controller and in model

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -9,6 +9,11 @@ import {
   getMissingOrEmptyFieldsErrorMessage,
 } from '../../utils/validation.utils';
 import { checkUserData } from '../../validation/User.validators';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const register = async (
   req: Request,
@@ -22,15 +27,25 @@ export const register = async (
      * TODO:
      * add avatar + avatar validation
      */
+    // default avatar path
+    const defaultAvatar = path.join(
+      __dirname,
+      '../../',
+      'uploads/placeholders/user-default-avatar.jpg',
+    );
 
-    // check for empty or missing fields
-    const missingOrEmptyFields = getMissingOrEmptyFields({
+    const user: IUser = {
       firstName,
       lastName,
       email,
       password,
       phone,
-    });
+      avatar: defaultAvatar,
+      role: UserRole.USER,
+    };
+
+    // check for empty or missing fields
+    const missingOrEmptyFields = getMissingOrEmptyFields(user);
 
     if (missingOrEmptyFields && missingOrEmptyFields.length > 0) {
       const errors = getMissingOrEmptyFieldsErrorMessage(missingOrEmptyFields);
@@ -41,22 +56,13 @@ export const register = async (
     }
 
     // data validation
-    const userDataErrors = checkUserData(req.body);
+    const userDataErrors = checkUserData(user);
 
     if (userDataErrors && userDataErrors.length > 0) {
       return res
         .status(400)
         .json({ message: i18n.t('auth.error.registerFailed'), userDataErrors });
     }
-
-    const user: IUser = {
-      firstName,
-      lastName,
-      email,
-      password,
-      phone,
-      role: UserRole.USER,
-    };
 
     const newUser = new User(user);
     const userInBase = await User.findOne({ email: newUser.email });
