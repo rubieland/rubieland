f8ef442ca22c1ecce0823ffd3c16180028efdaed
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-10 12:37:22 +0200

    feat(isMeMiddleware): send error if userId from req.params.userId does not belong to a user in database

--- backend/src/middlewares/user/isMe.middleware.ts
+++ backend/src/middlewares/user/isMe.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import i18n from '../../config/i18n';
+import User from '../../models/User.model';
 
 export const isMeMiddleware = async (
   req: Request,
@@ -11,6 +12,14 @@ export const isMeMiddleware = async (
     const sessionUserId = req.session.authUser?.id;
     // get the user id from the params
     const paramsUserId = req.params?.userId;
+
+    const userInBase = await User.findById(paramsUserId);
+
+    if (!userInBase) {
+      return res.status(404).json({
+        error: i18n.t('common.error.userDoesNotExist', { count: 0 }),
+      });
+    }
 
     // deny the access if a user tries to access the profile of another user
     if (sessionUserId !== paramsUserId) {
