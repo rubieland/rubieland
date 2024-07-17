eca74ad1994aab5d324d89dad8d641d535422a54
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-17 19:14:38 +0200

    WIP(date utils): edit date utils functions

--- backend/src/utils/date.utils.ts
+++ backend/src/utils/date.utils.ts
@@ -20,16 +20,25 @@ export const calculateAge = (birthDate: Date): number => {
   return age;
 };
 
+// calculate new date n years before the current date
+export const calculatePastDate = (years: number) => {
+  const currentDate = new Date();
+  const pastDate = new Date();
+  pastDate.setFullYear(currentDate.getFullYear() - years);
+  return pastDate;
+};
+
 /**
  * check if a date is valid and in ISO 8601 format
  * @param {Date} date - the date to validate
  * @returns {boolean} - returns true if the date is valid
  */
 export const isValidDate = (date: Date): boolean => {
-  return validator.isDate(date.toISOString(), {
-    format: 'DD-MM-YYYY',
-    strictMode: true,
-  });
+  if (isNaN(date.getTime())) {
+    return false;
+  }
+
+  return validator.isDate(date.toString());
 };
 
 /**
@@ -37,8 +46,8 @@ export const isValidDate = (date: Date): boolean => {
  * @param {Date} date - the date to validate
  * @returns {boolean} - returns true if the date is not in the future
  */
-export const isNotInFuture = (date: Date): boolean => {
-  return validator.isBefore(date.toISOString(), new Date().toISOString());
+export const isInFuture = (date: Date): boolean => {
+  return validator.isAfter(date.toISOString(), new Date().toISOString());
 };
 
 /**
@@ -47,8 +56,8 @@ export const isNotInFuture = (date: Date): boolean => {
  * @param {number} years - the number of years to check
  * @returns {boolean} - returns true if the date is not too old
  */
-export const isNotTooOld = (date: Date, years = 25): boolean => {
+export const isTooOld = (date: Date, years: number): boolean => {
   const limitDate = new Date();
   limitDate.setFullYear(limitDate.getFullYear() - years);
-  return validator.isAfter(date.toISOString(), limitDate.toISOString());
+  return validator.isBefore(date.toISOString(), limitDate.toISOString());
 };
