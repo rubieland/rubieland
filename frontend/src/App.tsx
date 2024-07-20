31f5889c7395f73d24b3eb04a51b86ceda98c104
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-20 19:58:07 +0200

    core: init i18next

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { useTranslation } from 'react-i18next';
+import i18n from './core/i18n';
 import './App.css';
 
 export const App = () => {
-  return <p>Hello world!</p>;
+  const { t } = useTranslation();
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div>
+      <p>{t('hello')}</p>
+      <button onClick={() => changeLanguage('fr')}>Français</button>
+      <button onClick={() => changeLanguage('en')}>English</button>
+    </div>
+  );
 };
 
 export default App;
