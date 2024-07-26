7e239e3c2e2e1ea2719d04598f37e4938437eb62
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-26 19:39:50 +0200

    utils: add utils for forms

---
+++ frontend/src/utils/string.utils.ts
@@ -0,0 +1,12 @@
+/**
+ * Adds an asterisk at the end of a text if there isn't already one
+ * @param text the text to which an asterisk will be added
+ * @returns the text with an asterisk added at the end, unless it already ends with an asterisk
+ */
+export const addAsterisk = (text: string): string => {
+  if (text.endsWith(' *') || text === '') {
+    return text;
+  } else {
+    return text + ' *';
+  }
+};
