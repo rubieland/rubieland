9d5c5b92a9c01f4e1fc0ad0048c0e17bb1b88f3f
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-29 21:38:52 +0200

    feat(Header logo): add a link to home page on logo

--- frontend/src/ui/components/Header/Header.tsx
+++ frontend/src/ui/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import { NavbarProvider } from '../Navbar/providers/NavbarProvider';
+import { Link } from '@tanstack/react-router';
 import Navbar from '../Navbar/Navbar';
 import Logo from '../Logo/Logo';
 import './Header.scss';
 
 const Header = () => {
   return (
-    <header className="header">
-      <Logo color="#7e57c2" />
+    <header>
+      <Link to="/">
+        <Logo color="#7e57c2" />
+      </Link>
       <NavbarProvider>
         <Navbar />
       </NavbarProvider>
