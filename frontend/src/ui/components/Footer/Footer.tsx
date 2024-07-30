f355ba612125981267826ae38083fd46f92305e7
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-30 21:04:07 +0200

    refactor(Footer): replace year by utils currentYear

--- frontend/src/ui/components/Footer/Footer.tsx
+++ frontend/src/ui/components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 import FooterSocialMediaBlock from './FooterSocialMediaBlock';
+import { currentYear } from '../../../utils/date.utils';
 import i18n from '../../../core/i18n';
 import './styles/Footer.scss';
 
 const Footer = () => {
-  const year = new Date().getFullYear();
   return (
     <footer>
       <FooterSocialMediaBlock />
       <p className="footer-copyright">
+        {i18n.t('footer.copyright', { year: currentYear })}
       </p>
     </footer>
   );
