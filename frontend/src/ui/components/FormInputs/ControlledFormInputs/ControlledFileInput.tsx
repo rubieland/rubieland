b50737e5f7880469c32a6efec60fb3c24a638e5f
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-08-02 16:49:36 +0200

    feat(ControlledFileInput): add inputFileRef and add preview when uploading a new file + default avatar svg

--- frontend/src/ui/components/FormInputs/ControlledFormInputs/ControlledFileInput.tsx
+++ frontend/src/ui/components/FormInputs/ControlledFormInputs/ControlledFileInput.tsx
@@ -1,25 +1,38 @@
 import { Controller, useFormContext } from 'react-hook-form';
+import { ChangeEvent, useRef, useState } from 'react';
+import DefaultAvatar from '../../Icons/DefaultAvatar';
+import Camera from '../../Icons/Camera';
 import FileInput from '../FileInput';
-import { ChangeEvent } from 'react';
 
 interface ControlledFileInputProps {
+  inputType: 'avatar' | 'blogArticlePicture';
+  currentAvatar?: string | null;
   acceptedMimetypes: string;
   isRequired?: boolean;
-  withLabel?: boolean;
   multiple?: boolean;
   label: string;
   name: string;
 }
 
 const ControlledFileInput = ({
+  currentAvatar = null,
   isRequired = false,
   acceptedMimetypes,
-  withLabel = true,
+  inputType,
   multiple = false,
   label,
   name,
 }: ControlledFileInputProps) => {
+  const inputFileRef = useRef<HTMLInputElement | null>(null);
+
   const { control } = useFormContext();
+  const [previewUrl, setPreviewUrl] = useState<string | ArrayBuffer | null>(
+    currentAvatar,
+  );
+  const classNames =
+    inputType === 'avatar'
+      ? `edit-avatar-input`
+      : `form-input edit-blog-article-picture-input`;
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement>,
@@ -28,9 +41,23 @@ const ControlledFileInput = ({
     const target = e.target as HTMLInputElement & {
       files: FileList;
     };
-    const selectedFile = target.files[0];
 
-    onChange([selectedFile]);
+    const selectedFile = target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setPreviewUrl(reader.result);
+    };
+
+    if (selectedFile) {
+      reader.readAsDataURL(selectedFile);
+      onChange([selectedFile]);
+    }
+  };
+
+  const handleClick = () => {
+    if (inputFileRef.current) {
+      inputFileRef.current.click();
+    }
   };
 
   return (
@@ -38,17 +65,29 @@ const ControlledFileInput = ({
       control={control}
       name={name}
       render={({ field: { onChange }, fieldState: { error } }) => (
-        <div className="form-input">
-          {withLabel && <label htmlFor={name}>{label}</label>}
+        <div className={classNames} onClick={handleClick}>
           <FileInput
+            onChange={(e) => handleChange(e, onChange)}
             acceptedMimetypes={acceptedMimetypes}
             isRequired={isRequired}
             isInvalid={!!error}
+            ref={inputFileRef}
             multiple={multiple}
-            onChange={(e) => handleChange(e, onChange)}
             label={label}
             name={name}
           />
+
+          <figure className="edit-avatar-figure">
+            {previewUrl && typeof previewUrl === 'string' ? (
+              <img src={previewUrl} alt={label} />
+            ) : (
+              <DefaultAvatar color="#fff" />
+            )}
+            <figcaption className="edit-avatar-figcaption">
+              <Camera color="#fff" />
+              <p className="caption-text">Modifier</p>
+            </figcaption>
+          </figure>
 
           {error && (
             <span className="input-error-message">{error.message}</span>
