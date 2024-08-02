16e178f9335596886165bf6458906c103a1ba639
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-08-02 12:22:58 +0200

    refactor(FileInput): remove multiple files handling and previews

--- frontend/src/ui/components/FormInputs/ControlledFormInputs/ControlledFileInput.tsx
+++ frontend/src/ui/components/FormInputs/ControlledFormInputs/ControlledFileInput.tsx
@@ -1,6 +1,6 @@
 import { Controller, useFormContext } from 'react-hook-form';
 import FileInput from '../FileInput';
-import { useState } from 'react';
+import { ChangeEvent } from 'react';
 
 interface ControlledFileInputProps {
   acceptedMimetypes: string;
@@ -12,16 +12,26 @@ interface ControlledFileInputProps {
 }
 
 const ControlledFileInput = ({
-  acceptedMimetypes,
   isRequired = false,
+  acceptedMimetypes,
   withLabel = true,
-  multiple = true,
+  multiple = false,
   label,
   name,
 }: ControlledFileInputProps) => {
   const { control } = useFormContext();
-  const [files, setFiles] = useState<File[]>([]);
-  const [previews, setPreviews] = useState<(string | ArrayBuffer | null)[]>([]);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    onChange: (...event: any[]) => void,
+  ) => {
+    const target = e.target as HTMLInputElement & {
+      files: FileList;
+    };
+    const selectedFile = target.files[0];
+
+    onChange([selectedFile]);
+  };
 
   return (
     <Controller
@@ -35,46 +45,10 @@ const ControlledFileInput = ({
             isRequired={isRequired}
             isInvalid={!!error}
             multiple={multiple}
-            onChange={(e) => {
-              const target = e.target as HTMLInputElement & {
-                files: FileList;
-              };
-
-              const selectedFiles = Array.from(target.files);
-              setFiles(selectedFiles);
-
-              const filePreviews = selectedFiles.map((file) => {
-                const reader = new FileReader();
-                reader.readAsDataURL(file);
-
-                return new Promise<string | ArrayBuffer | null>((resolve) => {
-                  reader.onload = () => {
-                    resolve(reader.result);
-                  };
-                });
-              });
-
-              Promise.all(filePreviews).then((previewResults) => {
-                setPreviews(previewResults);
-              });
-
-              onChange(selectedFiles);
-            }}
+            onChange={(e) => handleChange(e, onChange)}
             label={label}
             name={name}
           />
-
-          {previews.length > 0 && (
-            <div className="file-previews">
-              {previews.map((preview, index) =>
-                preview && typeof preview === 'string' ? (
-                  <div className="preview-img-container">
-                    <img key={index} src={preview} alt={`Preview ${index}`} />
-                  </div>
-                ) : null,
-              )}
-            </div>
-          )}
 
           {error && (
             <span className="input-error-message">{error.message}</span>
