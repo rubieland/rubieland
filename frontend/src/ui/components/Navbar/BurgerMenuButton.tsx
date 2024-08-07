8bf7d9cb6f195aaf569af2575491c596016b87e2
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-08-07 18:02:34 +0200

    refactor(BurgerMenuButton): create consts for classNames + add accessibility props

--- frontend/src/ui/components/Navbar/BurgerMenuButton.tsx
+++ frontend/src/ui/components/Navbar/BurgerMenuButton.tsx
@@ -1,23 +1,25 @@
 import { useNavbarContext } from './providers/NavbarProvider';
 import './styles/BurgerMenuButton.scss';
+import i18n from '../../../core/i18n';
 
 const BurgerMenuButton = () => {
   const { isOpen, toggleIsOpen } = useNavbarContext();
+  const burgerBtnClassName = isOpen
+    ? 'burger-menu-btn opened'
+    : 'burger-menu-btn';
+  const btnBarclassName = isOpen ? 'burger-menu-bar opened' : 'burger-menu-bar';
 
   return (
     <div
-      className={isOpen ? 'burger-menu-btn opened' : 'burger-menu-btn'}
+      aria-label={i18n.t('aria-labels.burger-btn')}
+      className={burgerBtnClassName}
+      aria-expanded={isOpen}
       onClick={toggleIsOpen}
+      role="button"
     >
-      <div
-        className={isOpen ? 'burger-menu-bar opened' : 'burger-menu-bar'}
-      ></div>
-      <div
-        className={isOpen ? 'burger-menu-bar opened' : 'burger-menu-bar'}
-      ></div>
-      <div
-        className={isOpen ? 'burger-menu-bar opened' : 'burger-menu-bar'}
-      ></div>
+      <div className={btnBarclassName}></div>
+      <div className={btnBarclassName}></div>
+      <div className={btnBarclassName}></div>
     </div>
   );
 };
