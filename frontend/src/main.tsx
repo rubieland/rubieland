7d42937323b436d35d4fc43a769fe266670f939d
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-09-02 15:56:21 +0200

    feat(main.tsx): add QueryClientProvider for api querries and ToastContainer for toasts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,19 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
+import { queryClient } from './api/reactQuery';
+import 'react-toastify/dist/ReactToastify.css';
 import ReactDOM from 'react-dom/client';
+import './core/i18n'; // init i18next
 import './assets/styles/index.scss';
 import './assets/styles/fonts.scss';
 import React from 'react';
 import App from './App';
-import './core/i18n'; // init i18next
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ToastContainer />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
