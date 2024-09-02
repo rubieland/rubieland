0c10a6dbbf07f33d03324ddc3bc143d74fc84ac0
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-09-02 16:04:21 +0200

    feat(Register page): add new RegisterForm component

--- frontend/src/ui/pages/Register/RegisterPage.tsx
+++ frontend/src/ui/pages/Register/RegisterPage.tsx
@@ -1,7 +1,13 @@
+import RegisterForm from './components/RegisterForm';
+import useRegister from './hooks/useRegister';
+
 const RegisterPage = () => {
+  const { onSubmit } = useRegister();
+
   return (
     <div>
       <p>RegisterPage</p>
+      <RegisterForm onSubmit={onSubmit} />
     </div>
   );
 };
