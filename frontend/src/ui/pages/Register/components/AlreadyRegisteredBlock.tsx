02ee6e5a9873824860c5dafab5b8e461e24e1749
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-09-04 11:28:00 +0200

    style(register page): change AlreadyRegisteredBlock style and change RegisterBlock h2 color

--- frontend/src/ui/pages/Register/components/AlreadyRegisteredBlock.tsx
+++ frontend/src/ui/pages/Register/components/AlreadyRegisteredBlock.tsx
@@ -5,10 +5,12 @@ import i18n from '../../../../core/i18n';
 const AlreadyRegisteredBlock = () => {
   return (
     <section className="already-registered-container">
-      <h2>{i18n.t('pages.register.alreadyRegisteredBlock.title')}</h2>
-      <Link className="link-to-login" to="/login">
-        {i18n.t('pages.register.alreadyRegisteredBlock.linkTitle')}
-      </Link>
+      <p>
+        {i18n.t('pages.register.alreadyRegisteredBlock.title')}{' '}
+        <Link className="link-to-login" to="/login">
+          {i18n.t('pages.register.alreadyRegisteredBlock.linkTitle')}
+        </Link>
+      </p>
     </section>
   );
 };
