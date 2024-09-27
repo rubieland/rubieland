75dcc73665a7ff7699cd059125207e4d6f42ae55
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-09-27 23:29:55 +0200

    style(header): add bottom box shadow on header when user scrolls

--- frontend/src/ui/components/Header/Header.tsx
+++ frontend/src/ui/components/Header/Header.tsx
@@ -1,14 +1,20 @@
+import { useScrollYPosition } from '../../../hooks/useScrollYPosition';
 import { NavbarProvider } from '../Navbar/providers/NavbarProvider';
 import colors from '../../../assets/styles/colors';
 import { Link } from '@tanstack/react-router';
 import i18n from '../../../core/i18n';
 import Navbar from '../Navbar/Navbar';
+import classNames from 'classnames';
 import Logo from '../Logo/Logo';
 import './Header.scss';
 
 const Header = () => {
+  const scrollYPosition = useScrollYPosition();
+  const className = classNames('app-header', {
+    'header-scrolled': scrollYPosition > 0,
+  });
   return (
-    <header className="app-header">
+    <header className={className}>
       <div className="app-logo-container">
         <Link to="/" aria-label={i18n.t('aria-labels.app-logo')} role="link">
           <Logo color={colors.primary} />
