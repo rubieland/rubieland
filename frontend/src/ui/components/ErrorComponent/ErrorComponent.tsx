b4658cd60f4c17a13a814e6914caeb33c8119c3c
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-02 00:44:33 +0200

    feat(ErrorComponent): add boolean to display a navigate back to home button

--- frontend/src/ui/components/ErrorComponent/ErrorComponent.tsx
+++ frontend/src/ui/components/ErrorComponent/ErrorComponent.tsx
@@ -1,14 +1,26 @@
 import { useTranslation } from 'react-i18next';
 import CustomButton from '../Button/CustomButton';
 import './ErrorComponent.scss';
+import { useNavigate } from '@tanstack/react-router';
 
 interface ErrorComponentProps {
+  showRedirectButton?: boolean;
   onRetry?: () => void;
   message: string;
 }
 
-const ErrorComponent = ({ message, onRetry }: ErrorComponentProps) => {
+const ErrorComponent = ({
+  message,
+  onRetry,
+  showRedirectButton,
+}: ErrorComponentProps) => {
   const { t } = useTranslation('translation', { keyPrefix: 'common' });
+  const navigate = useNavigate();
+
+  const navigateToHomepage = () => {
+    navigate({ to: '/' });
+  };
+
   return (
     <section className="error-component-section">
       <div className="error-box">
@@ -16,6 +28,13 @@ const ErrorComponent = ({ message, onRetry }: ErrorComponentProps) => {
         <p>{message}</p>
         {onRetry && (
           <CustomButton onClick={onRetry} title={t('retry')} style="error" />
+        )}
+        {showRedirectButton && (
+          <CustomButton
+            title={t('goBackToHomepage')}
+            onClick={navigateToHomepage}
+            style="error"
+          />
         )}
       </div>
     </section>
