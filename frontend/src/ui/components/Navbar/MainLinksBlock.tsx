24c8b976e89023a70a493697ca31be09e0b41120
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-02 18:16:36 +0200

    refactor(navbar): create MainLinksBlock component to reorder code properly

---
+++ frontend/src/ui/components/Navbar/MainLinksBlock.tsx
@@ -0,0 +1,65 @@
+import colors from '../../../assets/styles/colors';
+import { useTranslation } from 'react-i18next';
+import { Link } from '@tanstack/react-router';
+import { memo, useMemo } from 'react';
+import NavbarItem from './NavbarItem';
+
+interface MainLinksBlockProps {
+  isConnected: boolean;
+  hideMenu: () => void;
+}
+
+const activeProps = {
+  style: {
+    color: colors.primary,
+    fontWeight: 600,
+  },
+};
+
+// we use memo to prevent too many re-renders of the component
+const MainLinksBlock = memo(function ({
+  hideMenu,
+  isConnected,
+}: MainLinksBlockProps) {
+  const { t } = useTranslation('translation', { keyPrefix: 'nav' });
+
+  // we use useMemo to prevent re-creating the mainLinks array on every render
+  const mainLinks = useMemo(
+    () => [
+      {
+        title: t('home'),
+        to: '/',
+      },
+      {
+        title: t('about'),
+        to: '/about',
+      },
+      {
+        title: t('blog'),
+        to: '/blog',
+      },
+    ],
+    [t],
+  );
+
+  return (
+    <>
+      {mainLinks.map(({ title, to }) => (
+        <NavbarItem key={to} title={title} to={to} hideMenu={hideMenu} />
+      ))}
+
+      {isConnected && (
+        <Link
+          activeProps={activeProps}
+          className="navbar-link"
+          onClick={hideMenu}
+          to="/profile"
+        >
+          {t('profile')}
+        </Link>
+      )}
+    </>
+  );
+});
+
+export default MainLinksBlock;
--- frontend/src/ui/components/Navbar/Navbar.tsx
+++ frontend/src/ui/components/Navbar/Navbar.tsx
@@ -1,22 +1,11 @@
 import { useIsConnected } from '../../../stores/SessionStore';
-import colors from '../../../assets/styles/colors';
 import BurgerMenuButton from './BurgerMenuButton';
-import { useTranslation } from 'react-i18next';
+import { useCallback, useState } from 'react';
 import AuthLinksBlock from './AuthLinksBlock';
-import { useCallback, useMemo, useState } from 'react';
-import { Link } from '@tanstack/react-router';
-import NavbarItem from './NavbarItem';
+import MainLinksBlock from './MainLinksBlock';
 import './styles/Navbar.scss';
 
-const activeProps = {
-  style: {
-    color: colors.primary,
-    fontWeight: 600,
-  },
-};
-
 const Navbar = () => {
-  const { t } = useTranslation('translation', { keyPrefix: 'nav' });
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const isConnected: boolean = useIsConnected();
 
@@ -30,48 +19,12 @@ const Navbar = () => {
 
   const className: string = isOpen ? 'navbar' : 'navbar hidden';
 
-  const mainLinks = useMemo(
-    () => [
-      {
-        title: t('home'),
-        to: '/',
-      },
-      {
-        title: t('about'),
-        to: '/about',
-      },
-      {
-        title: t('blog'),
-        to: '/blog',
-      },
-    ],
-    [t],
-  );
-
   return (
     <nav className={className}>
       <BurgerMenuButton isOpen={isOpen} toggleIsOpen={toggleIsOpen} />
-      {mainLinks.map((link, i) => (
-        <NavbarItem
-          key={i}
-          to={link.to}
-          title={link.title}
-          hideMenu={hideMenu}
-        />
-      ))}
+      <MainLinksBlock isConnected={isConnected} hideMenu={hideMenu} />
 
-      {!isConnected ? (
-        <AuthLinksBlock hideMenu={hideMenu} />
-      ) : (
-        <Link
-          className="navbar-link"
-          activeProps={activeProps}
-          onClick={hideMenu}
-          to="/profile"
-        >
-          {t('profile')}
-        </Link>
-      )}
+      {!isConnected && <AuthLinksBlock hideMenu={hideMenu} />}
     </nav>
   );
 };
