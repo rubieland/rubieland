9e49e34681cf9f2a7236fd7bb11f300787779ead
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-04 11:18:25 +0200

    wording(BackOfficeLayout): rename Dashboard icon

--- frontend/src/ui/components/Layout/BackOfficeLayout.tsx
+++ frontend/src/ui/components/Layout/BackOfficeLayout.tsx
@@ -1,20 +1,21 @@
+import colors from '../../../assets/styles/colors';
 import { Outlet } from '@tanstack/react-router';
 import { LinkType } from '../../../types/links';
+import DashboardIcon from '../Icons/Dashboard';
 import { useTranslation } from 'react-i18next';
 import Sidebar from '../Sidebar/Sidebar';
-import Dashboard from '../Icons/Dashboard';
 import Book from '../Icons/Book';
-import colors from '../../../assets/styles/colors';
 
 const BackOfficeLayout = () => {
   const { t } = useTranslation('translation', {
     keyPrefix: 'pages.backOffice',
   });
+
   const links: LinkType[] = [
     {
       title: t('backOfficeSidebar.links.dashboard'),
       to: '/back-office',
-      icon: <Dashboard width={32} color={colors.grey80} />,
+      icon: <DashboardIcon width={32} color={colors.grey80} />,
     },
     {
       title: t('backOfficeSidebar.links.blog'),
