f2d64ebfcda2aaeffec07854726d3cbee1d69d95
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-05 17:58:40 +0200

    refactor: create types file + clean code

--- frontend/src/ui/pages/BackOffice/BackOfficeBlog/BackOfficeBlogPage.tsx
+++ frontend/src/ui/pages/BackOffice/BackOfficeBlog/BackOfficeBlogPage.tsx
@@ -1,6 +1,5 @@
 import { useGetAllPostsBackOffice } from '../../../../api/backOffice/blog/getAllPostsBackOffice';
 import ErrorComponent from '../../../components/ErrorComponent/ErrorComponent';
-import BackOfficePostCard from './components/BackOfficePostCard';
 import PageLoader from '../../../components/Loader/PageLoader';
 import './styles/BackOfficeBlogPage.scss';
 import {
@@ -12,30 +11,72 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import DataTable from '@/ui/components/Table/DataTable';
-import { log } from 'console';
-import { use } from 'i18next';
-import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 import { useMemo } from 'react';
+import { Post } from '@/models/posts/post.entity';
+import { CellProps, DataTypes } from '@/types/table';
 
 const BackOfficeBlogPage = () => {
   const { data: posts, isLoading, error, refetch } = useGetAllPostsBackOffice();
-  const columns = useMemo(
+
+  const columns = useMemo<ColumnDef<Post, DataTypes>[]>(
     () => [
       {
         header: 'ID',
         accessorKey: 'id',
-        cell: (props) => <p>{props.getValue()}</p>,
+        // Size is optional
+        size: 50,
+        cell: (props: CellProps<Post>) => <p>{props.getValue()}</p>,
       },
       {
         header: 'Title',
         accessorKey: 'title',
-        cell: (props) => <p>{props.getValue()}</p>,
+        size: 50,
+        cell: (props: CellProps<Post>) => <p>{props.getValue()}</p>,
+      },
+      {
+        header: 'Content',
+        accessorKey: 'content',
+        size: 50,
+        cell: (props: CellProps<Post>) => <p>{props.getValue()}</p>,
+      },
+      {
+        header: 'Picture',
+        accessorKey: 'picture',
+        size: 50,
+        cell: (props: CellProps<Post>) => (
+          <p>{props.getValue() ?? 'No picture'}</p>
+        ),
+      },
+      {
+        header: 'Is Published',
+        accessorKey: 'isPublished',
+        size: 50,
+        cell: (props: CellProps<Post>) => <p>{String(props.getValue())}</p>,
+      },
+      {
+        header: 'Created At',
+        accessorKey: 'createdAt',
+        size: 50,
+        cell: (props: CellProps<Post>) => <p>{props.getValue()}</p>,
+      },
+      {
+        header: 'Updated At',
+        accessorKey: 'updatedAt',
+        size: 50,
+        cell: (props: CellProps<Post>) => <p>{props.getValue()}</p>,
       },
     ],
 
     [],
   );
+
   const table = useReactTable({
     data: posts ?? [],
     columns,
@@ -46,44 +87,41 @@ const BackOfficeBlogPage = () => {
   if (error)
     return <ErrorComponent message={error.message} onRetry={refetch} />;
 
-  console.log('** table **', table.getHeaderGroups());
-
   return (
     <div className="bo-blog-page-main-container">
       <p>BackOfficeBlogPage</p>
-      {/* <Table>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <TableHead key={header.id}>
-                {header.column.columnDef.header}
-              </TableHead>
-            ))}
-          </TableRow>
-        ))}
-      </Table> */}
-
-      <table className="tw-w-full tw-caption-bottom tw-text-sm">
-        <thead>
-          <tr className="tw-border-b tw-transition-colors hover:tw-bg-neutral-100/50 data-[state=selected]:tw-bg-neutral-100 dark:hover:tw-bg-neutral-800/50 dark:data-[state=selected]:tw-bg-neutral-800">
-            <th className="tw-h-10 tw-px-2 tw-text-left tw-align-middle tw-font-medium tw-text-neutral-500 dark:tw-text-neutral-400">
-              ID
-            </th>
-            <th className="tw-h-10 tw-px-2 tw-text-left tw-align-middle tw-font-medium tw-text-neutral-500 dark:tw-text-neutral-400">
-              Title
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts &&
-            posts.map((post, i) => (
-              <tr key={i} className="tw-border-b">
-                <td className="tw-h-10 tw-px-2">{post.id}</td>
-                <td className="tw-h-10 tw-px-2">{post.title}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      <Table style={{ width: table.getTotalSize() }}>
+        {/* TODO: Replace with i18n */}
+        <TableCaption>Posts</TableCaption>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id} style={{ width: header.getSize() }}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext(),
+                  )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <TableCell
+                  key={cell.id}
+                  style={{ width: cell.column.getSize() }}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
 
       {/* <section className="posts-list">
         {posts &&
