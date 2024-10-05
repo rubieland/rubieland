941c0df5f4cbf95a6b497b99ea288f3230d4de8e
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-05 10:50:22 +0200

    refactor(blog): remove logic with admin => the blog only shows published posts no matter if user is admin or not

--- frontend/src/ui/pages/Blog/BlogPage.tsx
+++ frontend/src/ui/pages/Blog/BlogPage.tsx
@@ -2,7 +2,6 @@ import ErrorComponent from '../../components/ErrorComponent/ErrorComponent';
 import { useGetAllPosts } from '../../../api/blog/getAllPosts';
 import EmptyBlogSection from './components/EmptyBlogSection';
 import PageLoader from '../../components/Loader/PageLoader';
-import { useIsAdmin } from '../../../stores/SessionStore';
 import { Post } from '../../../models/posts/post.entity';
 import PostCardList from './components/PostCardList';
 import { useTranslation } from 'react-i18next';
@@ -15,16 +14,10 @@ import './styles/BlogPage.scss';
 const BlogPage = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'pages.blog' });
   const { data: allPosts, isLoading, error, refetch } = useGetAllPosts();
-  const isAdmin: boolean = useIsAdmin();
 
-  // a simple user must not see unpublished posts so we have to filter them
+  // we only show the published posts
   const publishedPosts: Post[] = filterPublishedPosts(allPosts) || [];
-
-  // if the user is a simple user, we need to check if there are published posts
   const hasNoPublishedPosts: boolean = checkIsBlogEmpty(publishedPosts);
-
-  // if the user is an admin, we don't need to filter the posts, we just check if there are posts already created
-  const isBlogEmpty: boolean = checkIsBlogEmpty(allPosts);
 
   if (isLoading) return <PageLoader isLoading={isLoading} />;
   if (error)
@@ -33,22 +26,10 @@ const BlogPage = () => {
   return (
     <div className="blog-page-main-container">
       <h2 className="main-title">{t('title')}</h2>
-      {isAdmin ? (
-        <>
-          {isBlogEmpty ? (
-            <EmptyBlogSection />
-          ) : (
-            <PostCardList posts={allPosts || []} />
-          )}
-        </>
+      {hasNoPublishedPosts ? (
+        <EmptyBlogSection />
       ) : (
-        <>
-          {hasNoPublishedPosts ? (
-            <EmptyBlogSection />
-          ) : (
-            <PostCardList posts={publishedPosts} />
-          )}
-        </>
+        <PostCardList posts={publishedPosts} />
       )}
     </div>
   );
