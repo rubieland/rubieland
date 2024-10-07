d05114ce9367b1e492d2394174ad1ff4671f2f00
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-07 18:06:52 +0200

    feat(back office blog page): add navigate to new CreatePostPage on click on button to create post

--- frontend/src/ui/pages/BackOffice/BackOfficeBlog/components/BackOfficeBlogPageHeader.tsx
+++ frontend/src/ui/pages/BackOffice/BackOfficeBlog/components/BackOfficeBlogPageHeader.tsx
@@ -1,20 +1,26 @@
 import CustomButton from '@/ui/components/Button/CustomButton';
 import CirclePlus from '@/ui/components/Icons/CirclePlus';
+import { useNavigate } from '@tanstack/react-router';
 import '../styles/BackOfficeBlogPageHeader.scss';
 import { useTranslation } from 'react-i18next';
 import colors from '@/assets/styles/colors';
 
 const BackOfficeBlogPageHeader = () => {
+  const navigate = useNavigate({ from: '/back-office/blog/' });
   const { t } = useTranslation('translation', {
     keyPrefix: 'pages.backOffice.blog',
   });
+
+  const handleNavigate = () => {
+    navigate({ to: '/back-office/blog/create-post' });
+  };
 
   return (
     <header className="bo-blog-section-header">
       <h2>{t('title')}</h2>
       <CustomButton
         icon={<CirclePlus width={24} height={24} stroke={colors.primary} />}
-        onClick={() => console.log(t('createPost'))}
+        onClick={handleNavigate}
         title={t('createPost')}
         iconPosition="right"
         style="primary"
