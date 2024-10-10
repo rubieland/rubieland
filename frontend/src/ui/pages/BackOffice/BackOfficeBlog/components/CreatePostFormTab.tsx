a614feba34437c71abad9b5f6b1ee1efa63388b0
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-10 18:29:07 +0200

    refactor: move navigate in form parent

--- frontend/src/ui/pages/BackOffice/BackOfficeBlog/components/CreatePostFormTab.tsx
+++ frontend/src/ui/pages/BackOffice/BackOfficeBlog/components/CreatePostFormTab.tsx
@@ -1,6 +1,7 @@
 import { CreatePostSchemaFormData } from '../../hooks/useCreatePostFormValidation';
 import useCreateNewPost from '../../hooks/useCreateNewPost';
 import CreatePostTabsHeader from './CreatePostTabsHeader';
+import { useNavigate } from '@tanstack/react-router';
 import { isFormValid } from '@/utils/form.utils';
 import { UseFormReturn } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -17,6 +18,8 @@ const CreatePostFormTab = ({ formMethods }: CreatePostFormTabProps) => {
     keyPrefix: 'pages.backOffice.blog',
   });
 
+  const navigate = useNavigate();
+
   const { onSubmit } = useCreateNewPost();
   const { handleSubmit, watch, formState, reset } = formMethods;
   const watchedValues = watch(['title', 'content']);
@@ -24,12 +27,13 @@ const CreatePostFormTab = ({ formMethods }: CreatePostFormTabProps) => {
 
   const isSubmitSuccessful = formState.isSubmitSuccessful;
 
-  // reset form when the post is successfully created
+  // reset form when the post is successfully created and navigate back to the blog page
   useEffect(() => {
     if (isSubmitSuccessful) {
       reset();
+      navigate({ to: '/back-office/blog' });
     }
-  }, [isSubmitSuccessful, reset]);
+  }, [isSubmitSuccessful, reset, navigate]);
 
   return (
     <section>
