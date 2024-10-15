7527c530f4844366327b1941839a805c3b31e8e8
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-15 14:20:04 +0200

    refactor(About page): separate About sections in components

--- frontend/src/ui/pages/About/AboutPage.tsx
+++ frontend/src/ui/pages/About/AboutPage.tsx
@@ -1,45 +1,34 @@
+import PrestationsSection from './components/PrestationsSection';
+import AboutSection from './components/AboutSection';
 import { useTranslation } from 'react-i18next';
 import { Helmet } from 'react-helmet-async';
 import './AboutPage.scss';
 
 const AboutPage = () => {
   const { t } = useTranslation();
+
   return (
     <div className="about-main-container">
       <Helmet>
         <title>{t('SEO.about.title')}</title>
         <meta name="description" content={t('SEO.about.description')} />
       </Helmet>
+
       <h2 className="main-title">{t('pages.about.title')}</h2>
       <div className="about-sections-container">
-        <section className="about-section" id="about-rubieland">
-          <h3>{t('pages.about.sections.aboutRubieland.title')}</h3>
-          <div className="about-section-content">
-            <p>{t('pages.about.sections.aboutRubieland.content')}</p>
-          </div>
-        </section>
-
-        <section className="about-section" id="amenities">
-          <h3>{t('pages.about.sections.facilities.title')}</h3>
-          <div className="about-section-content">
-            <p>{t('pages.about.sections.facilities.content')}</p>
-          </div>
-        </section>
-
-        <section className="about-section" id="location">
-          <h3>{t('pages.about.sections.location.title')}</h3>
-          <div className="about-section-content">
-            <p>{t('pages.about.sections.location.content')}</p>
-          </div>
-        </section>
-
-        <section className="about-section" id="services">
-          <h3>{t('pages.about.sections.prestations.title')}</h3>
-          <div className="about-section-content">
-            <p>{t('pages.about.sections.prestations.content.fullDay')}</p>
-            <p>{t('pages.about.sections.prestations.content.halfDay')}</p>
-          </div>
-        </section>
+        <AboutSection
+          title={t('pages.about.sections.aboutRubieland.title')}
+          content={t('pages.about.sections.aboutRubieland.content')}
+        />
+        <AboutSection
+          title={t('pages.about.sections.amenities.title')}
+          content={t('pages.about.sections.amenities.content')}
+        />
+        <AboutSection
+          title={t('pages.about.sections.location.title')}
+          content={t('pages.about.sections.location.content')}
+        />
+        <PrestationsSection />
       </div>
     </div>
   );
