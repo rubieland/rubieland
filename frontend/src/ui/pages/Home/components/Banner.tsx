b2208e8e78a6cb57856981c6ea73fb6a7cb972dd
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-15 12:26:05 +0200

    feat(Banner): add Text and CTA and translation keys

--- frontend/src/ui/pages/Home/components/Banner.tsx
+++ frontend/src/ui/pages/Home/components/Banner.tsx
@@ -1,8 +1,38 @@
+import mediumBannerImg from '@/assets/illustrations/banner_medium.jpg';
+import largeBannerImg from '@/assets/illustrations/banner_large.jpg';
+import ResponsiveImage from '@/ui/components/Image/ResponsiveImage';
+import CustomButton from '@/ui/components/Button/CustomButton';
+import { useNavigate } from '@tanstack/react-router';
+import { useTranslation } from 'react-i18next';
 import '../styles/Banner.scss';
 
+const imageSources = [{ media: '(min-width: 768px)', srcSet: largeBannerImg }];
+
 const Banner = () => {
-  // TODO: add text and CTA
-  return <section className="banner" />;
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate({ from: '/', to: '/about' });
+  };
+
+  return (
+    <section className="banner">
+      <ResponsiveImage
+        defaultSrc={mediumBannerImg}
+        srcSet={imageSources}
+        alt={t('pages.home.banner.bannerImgAlt')}
+      />
+      <div className="banner-content">
+        <h1>{t('pages.home.banner.bannerTitle')}</h1>
+        <p>{t('pages.home.banner.bannerText')}</p>
+        <CustomButton
+          title={t('pages.home.banner.bannerCTA')}
+          onClick={handleNavigate}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- frontend/src/ui/pages/Home/components/PresentationSection.tsx
+++ frontend/src/ui/pages/Home/components/PresentationSection.tsx
@@ -1,38 +1,34 @@
-import imgSrc from '../../../../assets/illustrations/happy-dog.jpg';
+import happyDogMediumImg from '@/assets/illustrations/happy_dog_medium.jpg';
+import happyDogLargeImg from '@/assets/illustrations/happy_dog_large.jpg';
+import ResponsiveImage from '@/ui/components/Image/ResponsiveImage';
+import { useTranslation } from 'react-i18next';
 import { Link } from '@tanstack/react-router';
 import '../styles/PresentationSection.scss';
-import { t } from 'i18next';
+
+const imageSources = [
+  { media: '(min-width: 580px)', srcSet: happyDogLargeImg },
+];
 
 const PresentationSection = () => {
+  const { t } = useTranslation();
+
   return (
     <section className="presentation-section">
       <div className="presentation-content">
         <div className="text-column">
-          {/* TODO: Add i18n translation and replace the title and paragraphs texts */}
-          <h2>Bienvenue chez Rubieland</h2>
-          <p>
-            Rubieland, c'est la garderie où votre compagnon à quatre pattes se
-            sentira comme chez lui. Que ce soit pour une journée ou un séjour
-            plus long, nos services sont conçus pour garantir le bien-être de
-            vos chiens. Nos infrastructures sont pensées pour offrir un
-            environnement ludique et sécurisé.
-          </p>
-          <p>
-            Nous croyons en un accompagnement personnalisé pour chaque chien,
-            avec des activités adaptées à leurs besoins et à leurs envies.
-            Faites-nous confiance pour prendre soin de vos compagnons avec tout
-            l'amour qu'ils méritent.
-          </p>
+          <h2>{t('pages.home.presentation.title')}</h2>
+          <p>{t('pages.home.presentation.firstDesc')}</p>
+          <p>{t('pages.home.presentation.secondDesc')}</p>
           <Link className="read-more-btn btn btn-primary" to="/about">
             {t('common.readMore')}
           </Link>
         </div>
 
         <div className="image-column">
-          <img
-            src={imgSrc}
-            // TODO: Replace alt text
-            alt="Chien heureux à Rubieland"
+          <ResponsiveImage
+            defaultSrc={happyDogMediumImg}
+            srcSet={imageSources}
+            alt=""
           />
         </div>
       </div>
