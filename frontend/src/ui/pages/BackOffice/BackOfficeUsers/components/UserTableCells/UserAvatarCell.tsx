b969c219f283193e658753dce2d78f9282360368
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-17 22:43:58 +0200

    feat(user table): add styles and avatar, add api avatar path in vite and env config

--- frontend/src/ui/pages/BackOffice/BackOfficeUsers/components/UserTableCells/UserAvatarCell.tsx
+++ frontend/src/ui/pages/BackOffice/BackOfficeUsers/components/UserTableCells/UserAvatarCell.tsx
@@ -1,9 +1,28 @@
+import { API_URL, API_USERS_AVATARS_PATH } from '@/core/envConfig';
+import { useTranslation } from 'react-i18next';
+import '../../styles/UserAvatarCell.scss';
+
 interface UserAvatarCellProps {
   avatar: string | null;
 }
 
 const UserAvatarCell = ({ avatar }: UserAvatarCellProps) => {
-  return <span className="table-cell-content">{avatar}</span>;
+  const { t } = useTranslation('translation', {
+    keyPrefix: 'pages.backOffice.tables.users',
+  });
+
+  return (
+    <div className="user-avatar-cell">
+      {avatar ? (
+        <img
+          src={`${API_URL}/${API_USERS_AVATARS_PATH}/${avatar}`}
+          alt={t('avatarAlt')}
+        />
+      ) : (
+        <span className="no-image">{t('noAvatar')}</span>
+      )}
+    </div>
+  );
 };
 
 export default UserAvatarCell;
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_API_BLOG_PICTURES_PATH: string;
+  readonly VITE_API_USERS_AVATARS_PATH: string;
 }
 
 interface ImportMeta {
