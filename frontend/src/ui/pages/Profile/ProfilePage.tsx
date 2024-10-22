a11493c40a9ae3e2775c6cbd11f5d2bc006a7026
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-22 15:31:45 +0200

    refactor+feat(logout): create LogoutModal, hook to handle this modal and LogoutSection

--- frontend/src/ui/pages/Profile/ProfilePage.tsx
+++ frontend/src/ui/pages/Profile/ProfilePage.tsx
@@ -1,26 +1,19 @@
-import CustomButton from '@/ui/components/Button/CustomButton';
-import PageLoader from '@/ui/components/Loader/PageLoader';
+import Separator from '@/ui/components/Separator/Separator';
 import SettingsSection from './components/SettingsSection';
 import ProfileSection from './components/ProfileSection';
-import LogoutIcon from '@/ui/components/Icons/Logout';
+import LogoutSection from './components/LogoutSection';
 import { useNavigate } from '@tanstack/react-router';
 import { useUserInfo } from '@/stores/SessionStore';
 import { useTranslation } from 'react-i18next';
 import { Helmet } from 'react-helmet-async';
-import useLogout from '@/hooks/useLogout';
 import './styles/ProfilePage.scss';
 
 const ProfilePage = () => {
   const { t } = useTranslation();
   const user = useUserInfo();
   const navigate = useNavigate();
-  const { handleLogout, isPending } = useLogout();
 
   if (!user) return navigate({ to: '/login' });
-
-  if (isPending) return <PageLoader isLoading={isPending} />;
-
-  // TODO: add logout modal to ask user for confirmation before logging out
 
   return (
     <div className="profile-page-main-container">
@@ -31,16 +24,8 @@ const ProfilePage = () => {
 
       <ProfileSection existingProfileData={user} />
       <SettingsSection />
-      <div className="logout-button">
-        <CustomButton
-          icon={<LogoutIcon width={16} height={16} />}
-          title={t('common.logout')}
-          onClick={handleLogout}
-          iconPosition="right"
-          style="error"
-          outlined
-        />
-      </div>
+      <Separator />
+      <LogoutSection />
     </div>
   );
 };
