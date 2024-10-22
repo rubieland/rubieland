7e7cbb63e70a9d95f8f0f31e87979a3aca0aa919
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-10-22 15:32:03 +0200

    refactor(sidebar): use new logout modal

--- frontend/src/ui/components/Sidebar/SidebarLogoutButton.tsx
+++ frontend/src/ui/components/Sidebar/SidebarLogoutButton.tsx
@@ -1,7 +1,9 @@
+import useLogoutModal from '@/ui/pages/Profile/hooks/useLogoutModal';
+import { memo, useCallback, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import LogoutModal from '../Modal/LogoutModal';
+import PageLoader from '../Loader/PageLoader';
 import colors from '@/assets/styles/colors';
-import { memo, useCallback } from 'react';
-import useLogout from '@/hooks/useLogout';
 import LogoutIcon from '../Icons/Logout';
 import classNames from 'classnames';
 
@@ -13,7 +15,15 @@ interface SidebarLogoutButtonProps {
 const SidebarLogoutButton = memo(
   ({ hideSidebar, isOpen }: SidebarLogoutButtonProps) => {
     const { t } = useTranslation();
-    const { handleLogout } = useLogout();
+
+    const modalRef = useRef<HTMLDialogElement>(null);
+    const {
+      handleLogoutAndCloseModal,
+      handleCloseModal,
+      handleOpenModal,
+      isModalOpen,
+      isPending,
+    } = useLogoutModal();
 
     const className = classNames('sidebar-link sidebar-logout-button', {
       opened: isOpen,
@@ -21,8 +31,8 @@ const SidebarLogoutButton = memo(
 
     const handleClick = useCallback(() => {
       hideSidebar();
-      handleLogout();
-    }, [hideSidebar, handleLogout]);
+      handleOpenModal();
+    }, [hideSidebar, handleOpenModal]);
 
     const toggleMenuOnKeyDown = useCallback(
       () => (e: React.KeyboardEvent) => {
@@ -36,18 +46,28 @@ const SidebarLogoutButton = memo(
       [handleClick, hideSidebar],
     );
 
+    if (isPending) return <PageLoader isLoading={isPending} />;
+
     return (
-      <li
-        aria-label={t('common.logout')}
-        onKeyDown={toggleMenuOnKeyDown}
-        className={className}
-        onClick={handleClick}
-        tabIndex={0}
-        role="link"
-      >
-        <LogoutIcon width={24} height={24} stroke={colors.red} />
-        <p>{t('common.logout')}</p>
-      </li>
+      <>
+        <li
+          aria-label={t('common.logout')}
+          onKeyDown={toggleMenuOnKeyDown}
+          className={className}
+          onClick={handleClick}
+          tabIndex={0}
+          role="link"
+        >
+          <LogoutIcon width={24} height={24} stroke={colors.red} />
+          <p>{t('common.logout')}</p>
+        </li>
+        <LogoutModal
+          onLogout={handleLogoutAndCloseModal}
+          onClose={handleCloseModal}
+          isOpen={isModalOpen}
+          modalRef={modalRef}
+        />
+      </>
     );
   },
 );
